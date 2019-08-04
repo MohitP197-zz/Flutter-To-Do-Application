2be63b66967c7a983933acd872056432c6e3c2cc
Mahendra Paudel <paudel823@gmail.com>
2019-08-04 08:26:35 +0545

    Updated todo_item model

--- lib/model/todo_item.dart
+++ lib/model/todo_item.dart
@@ -5,10 +5,66 @@ class ToDoItem extends StatelessWidget {
   String _itemName;
   String _dateCreated;
   int _id;
+
+  // Constructor
+  ToDoItem(this._itemName, this._dateCreated);
+
+  // Another constructor for mapping the object with what is obtained from database
+  // Setting all the internal fields to the object we are getting so that they become objects
+  ToDoItem.map(dynamic obj) {
+    this._itemName = obj["itemName"];
+    this._dateCreated = obj["dateCreated"];
+    this._id = obj["id"];
+  }
+
+  // Getters
+  String get itemName => _itemName;
+  String get dateCreated => _dateCreated;
+  int get id => _id;
+
+  Map<String, dynamic> toMap() {
+    var map = new Map<String, dynamic>();
+    map["itemName"] = _itemName;
+    map["dateCreated"] = _dateCreated;
+
+    if (_id != null) {
+      map["id"] = _id;
+    }
+    return map;
+  }
+
+  ToDoItem.fromMap(Map<String, dynamic> map) {
+    this._itemName = map["itemName"];
+    this._dateCreated = map["dateCreated"];
+    this._id = map["id"];
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
-      
+      margin: const EdgeInsets.all(8.0),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Text(
+            _itemName,
+            style: TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+                fontSize: 16.9),
+          ),
+          Container(
+            margin: const EdgeInsets.only(top: 5.0),
+            child: Text(
+              "Created on: $_dateCreated",
+              style: TextStyle(
+                  color: Colors.white70,
+                  fontSize: 13.5,
+                  fontStyle: FontStyle.italic),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
