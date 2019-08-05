117c258b6ee3b482f0455a6a99308ba2cd160cf0
Mahendra Paudel <paudel823@gmail.com>
2019-08-05 08:30:40 +0545

    Fixed designs

--- lib/model/todo_item.dart
+++ lib/model/todo_item.dart
@@ -46,23 +46,28 @@ class ToDoItem extends StatelessWidget {
       child: Row(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
-          Text(
-            _itemName,
-            style: TextStyle(
-                color: Colors.white,
-                fontWeight: FontWeight.bold,
-                fontSize: 16.9),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text(
+                _itemName,
+                style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 16.9),
+              ),
+              Container(
+                margin: const EdgeInsets.only(top: 5.0),
+                child: Text(
+                  "Created on: $_dateCreated",
+                  style: TextStyle(
+                      color: Colors.white70,
+                      fontSize: 13.5,
+                      fontStyle: FontStyle.italic),
+                ),
+              )
+            ],
           ),
-          Container(
-            margin: const EdgeInsets.only(top: 5.0),
-            child: Text(
-              "Created on: $_dateCreated",
-              style: TextStyle(
-                  color: Colors.white70,
-                  fontSize: 13.5,
-                  fontStyle: FontStyle.italic),
-            ),
-          )
         ],
       ),
     );
